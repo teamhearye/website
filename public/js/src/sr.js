958b3ab3cb88cc2e9981cd7321e39337fa4e8577
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-05-06 00:14:02 -0500

    added overflow-x

--- public/js/src/sr.js
+++ public/js/src/sr.js
@@ -19,3 +19,32 @@ window.sr = ScrollReveal();
 sr.reveal('.explore', featureRight);
 sr.reveal('.tailor', featureLeft);
 sr.reveal('.create', { delay: 200, scale: 0.9 });
+
+
+// var controller = new ScrollMagic({
+//     globalSceneOptions: {
+//         triggerHook: "onLeave"
+//     }
+// });
+
+// var pinani = new TimelineMax()
+//     // panel wipe uno
+//     .add(TweenMax.to("#hero", 1, {transform: "translateY(0)"}))
+
+//     // panel slide translateX
+//     .add(TweenMax.to("#slide-dos", 1, {transform: "translateX(0)"}))
+
+//     // panel unpinned
+//     .add(TweenMax.from("#unpin", .5, {top: "100%"}));
+
+
+// // panel section pin
+// var scene = new ScrollScene({
+//         triggerElement: "header#hero",
+//         duration: 1100
+//     })
+//     .setTween(pinani)
+//     .setPin("header#hero")
+//     .addTo(controller);
+
+//         scene.addIndicators();
